1b11dd10e9d2d6deb23f22e175c9b7a26b262577
Julian Metzger <julianjometzger@gmail.com>
2023-04-18 21:38:12 +0200

    :(

--- src/app/api/verify-email/finish/route.ts
+++ src/app/api/verify-email/finish/route.ts
@@ -12,7 +12,7 @@ export async function POST(request: Request) {
       });
     }
 
-    const data = await prisma.user.findUniqueOrThrow({
+    const data = await prisma.user.findUnique({
       where: {
         email,
       },
@@ -21,7 +21,7 @@ export async function POST(request: Request) {
       },
     });
 
-    if (data.AuthCode !== inputCode) {
+    if (data?.AuthCode !== inputCode) {
       return new Response("The verification code doesn't match.", {
         status: 400,
       });
