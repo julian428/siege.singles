db117f99df7a656a45daa025bd7d0414352b1d8b
Julian Metzger <julianjometzger@gmail.com>
2023-04-22 09:44:56 +0200

    deleted some unused js

--- src/app/auth/signup/page.tsx
+++ src/app/auth/signup/page.tsx
@@ -1,17 +1,11 @@
 import SignUpForm from "@/components/auth/signupForm";
 import PageTitle from "@/components/ui/title";
-import { authOptions } from "@/lib/auth";
-import { getServerSession } from "next-auth";
-import { redirect } from "next/navigation";
 
 export const metadata = {
   title: "Siege Singles | SignUp",
 };
 
 export default async function SignUpPage() {
-  const session = await getServerSession(authOptions);
-  if (session) redirect("/singles/match");
-
   return (
     <>
       <header>
